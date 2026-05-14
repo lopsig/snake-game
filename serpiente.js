b98fb176d5e9f93f48bef2e7020714ced81d9850
lopsig <jonathan.ls.trabajo@gmail.com>
2026-05-13 21:50:16 -0500

    refactor: clean up and reorganize drawing functions in JavaScript

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,39 @@
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+//CONSTANTES
+const cellSize = 25;
 
+function drawAll() {
+  cleanCanvas();
+  drawBoard();
+}
 
-    
+function cleanCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+// DIBUJAR CUADRICULA
+const drawBoard = () => {
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+  ctx.strokeStyle = "gray";
+  
+  for (let i = 25; i < canvas.width; i += cellSize) {
+    ctx.beginPath();
+    ctx.moveTo(i, 0)
+    ctx.lineTo(i, canvas.height)
+    ctx.stroke()
+  }
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+  for (let j = 0; j < canvas.height; j += cellSize) {
+    ctx.beginPath();
+    ctx.moveTo(0, j);
+    ctx.lineTo(canvas.width, j);
+    ctx.stroke();
+  }
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+  
+};
 
-
-
+drawAll();
